001d1165af00b9d545de9f6f61588e763e9cb5c8
go <goito.722@icloud.com>
2024-04-29 21:53:13 +0900

    環境構築をした

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home:React.FC = () => {
+  return (
+    <div className='text-slate-400'>
+      home
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/pages/Team.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Team:React.FC = () => {
+  return (
+    <div>
+      team
+    </div>
+  )
+}
+
+export default Team;
