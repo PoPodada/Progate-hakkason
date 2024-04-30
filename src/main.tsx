34dc723b20de0e6ae151a328c160a8ab46209989
ibukani <ibukani777@gmail.com>
2024-04-30 23:07:31 +0900

    add: firebase の作成

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home  from './pages/Home.tsx'
-import Team from './pages/Team.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Team from "./pages/Team.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { firebaseApp } from "./firebase.ts";
 
 const router = createBrowserRouter([
-    { path: '/', element:<Home /> },
-    { path: '/team', element: <Team /> },
-],)
+  { path: "/", element: <Home /> },
+  { path: "/team", element: <Team /> },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+firebaseApp;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
