8bc1ec70e9b75efe96c001726c5feae6f50b889e
ibukani <ibukani777@gmail.com>
2024-05-01 17:38:01 +0900

    デバッグ用の処理を削除

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import "./index.css";
 import Home from "./pages/Home.tsx";
 import Team from "./pages/Team.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { firebaseApp } from "./firebase.ts";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/team", element: <Team /> },
 ]);
-
-firebaseApp;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
