45aa148ba43cd1d64f6e4661c1b840d932f28a02
haru827 <haruhanamio@keio.jp>
2024-05-01 20:19:51 +0900

    TeamPreview.tsxにチーム一覧の要素を作った

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import TeamPreview from './TeamPreview'
 const Home:React.FC = () => {
   return (
-    <div className='text-slate-400'>
-      home
+    <div className='max-w-[900px] mx-auto mt-16'>
+      <h2 className='text-2xl font-bold'>入っているチーム一覧</h2>
+      <TeamPreview></TeamPreview>
+    
     </div>
   )
 }
