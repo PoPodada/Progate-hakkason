53c499ac70c01578c85db388a052c59e80f7957f
haru <hinagoro5656@gmail.com>
2024-05-02 20:04:00 +0900

    チーム作成モーダルを作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import CreateTeamModal from "./CreateTeamModal";
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
   return (
-    <div className='text-slate-400'>
-      home
+    <div className="max-w-[900px] mx-auto mt-6">
+      <h1 className="text-4xl">Home</h1>
+      <CreateTeamModal />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
