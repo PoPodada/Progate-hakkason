58afca818a088b1a76e4279ff8e8a1129afe6f1e
go <goito.722@icloud.com>
2024-05-04 15:47:14 +0900

    meetingcardのidでプロパティエラーが出てしまうのを修正

--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -5,17 +5,24 @@ const MeetingCard = (props:any) => {
 
     <div className=" bg-white  rounded-lg">
       <div className=" bg-neutral-0 py-6 px-8 rounded-lg text-xl tracking-wider ">
-        <p className="text-2xl mb-2">会議{props.detail.id}</p>
-        <p className="text-xl mb-1">日程：{props.detail.time}</p>
-        <p className="text-xl mb-1">メンバー：
         {
-          props.detail.members.map((member:any)=>{
-            console.log(member)
-            return(`${member},`)
+          props.detail.id ? 
+          <>
+          <p className="text-2xl mb-2">会議{props.detail.id}</p>
+          <p className="text-xl mb-1">日程：{props.detail.time}</p>
+          <p className="text-xl mb-1">メンバー：
+          {
+            props.detail.members.map((member:any)=>{
+              console.log(member)
+              return(`${member},`)
+            }
+            )
           }
-          )
+          </p> 
+          </>:
+          ""
         }
-        </p>
+        
 
         <div className="flex gap-x-2"> {/**flexで横並び、gapで間を指定、↓の丸にはアイコンが入る */}
           <div className=" bg-neutral-300 h-12 w-12 rounded-full mt-2"></div>
