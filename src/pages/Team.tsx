19afc80d4351c4c57a2a4a2baf30831c16d40dd9
go <goito.722@icloud.com>
2024-05-04 13:54:06 +0900

    チーム画面での会議一覧の表示 日程をフォーマットする必要がある

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -3,8 +3,21 @@ import React from "react";
 import MeetingCreate from "../components/MeetingCreate";
 
 import MeetingCard from "../components/MeetingCard";
+import data from "../sampleData/teamData.json";
+import { useEffect } from "react";
+import { useState } from "react";
+import { meeting } from "../pages/Home";
 
+//propsで会議idを受け取る
 const Team: React.FC = () => {
+  const [meetings,setMeetings] = useState<meeting[]>();
+  useEffect(() => {
+    let teamid = "1";
+    const teamData = data;
+    const MeetingList = teamData.filter((data) => data.id === teamid);
+    console.log(MeetingList[0].meetings)
+    setMeetings(MeetingList[0].meetings)
+  }, []);
   return (
     <div>
       team
@@ -12,9 +25,13 @@ const Team: React.FC = () => {
         <MeetingCreate></MeetingCreate>
         <h2 className="text-2xl font-bold mt-40">会議一覧</h2>
         <div className=" bg-neutral-300 py-12 px-12 rounded-md mt-2 space-y-10">
-          <MeetingCard></MeetingCard>
-          <MeetingCard></MeetingCard>
-          <MeetingCard></MeetingCard>
+          {
+            meetings? meetings.map((meeting)=>{
+              return(
+                <MeetingCard detail={meeting}></MeetingCard>
+              )
+            }):""
+          }
         </div>
       </div>
     </div>
