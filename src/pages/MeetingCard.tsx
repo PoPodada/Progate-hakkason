3c7463c9e0c9ba2a4f9a55c74e47ee6bd77e92d6
go <goito.722@icloud.com>
2024-05-04 13:11:43 +0900

    ユーザーが参加する会議予定一覧

--- src/pages/MeetingCard.tsx
+++ src/pages/MeetingCard.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 
-const MeetingCard: React.FC = () => {
+const MeetingCard = (props:any) => {
   return (
     <div className=" bg-white  rounded-lg">
       <div className=" bg-neutral-0 py-6 px-8 rounded-lg text-xl tracking-wider ">
-        <p className="text-2xl mb-2">会議1</p>
-        <p className="text-xl mb-1">日程：12/24 12:00~</p>
-        <p className="text-xl mb-1">メンバー：</p>
+        <p className="text-2xl mb-2">会議{props.detail.id}</p>
+        <p className="text-xl mb-1">日程：{props.detail.time}</p>
+        <p className="text-xl mb-1">メンバー：{props.detail.id}</p>
         <div className="flex gap-x-2"> {/**flexで横並び、gapで間を指定、↓の丸にはアイコンが入る */}
           <div className=" bg-neutral-300 h-12 w-12 rounded-full mt-2"></div>
           <div className=" bg-neutral-300 h-12 w-12 rounded-full mt-2"></div>
