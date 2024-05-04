083f529afd6964da019e2fc6113aa134c0ae8865
haru <hinagoro5656@gmail.com>
2024-05-04 16:52:34 +0900

    divの閉じタグ追加

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -9,18 +9,15 @@ import { useState } from "react";
 import { meeting } from "../pages/Home";
 import { useLocation } from "react-router-dom";
 
-
-
 //propsで会議idを受け取る予定
 const Team: React.FC = () => {
-
-  const [meetings,setMeetings] = useState<meeting[]>();
+  const [meetings, setMeetings] = useState<meeting[]>();
   useEffect(() => {
     let teamid = "1";
     const teamData = data;
     const MeetingList = teamData.filter((data) => data.id === teamid);
-    console.log(MeetingList[0].meetings)
-    setMeetings(MeetingList[0].meetings)
+    console.log(MeetingList[0].meetings);
+    setMeetings(MeetingList[0].meetings);
   }, []);
 
   const locationState = useLocation();
@@ -33,7 +30,6 @@ const Team: React.FC = () => {
       alert("URLのコピーに失敗しました");
     }
   };
-
 
   return (
     <div>
@@ -81,21 +77,19 @@ const Team: React.FC = () => {
           <div className=" bg-neutral-300 h-12 w-12 rounded-full "></div>
           <div className=" bg-neutral-300 h-12 w-12 rounded-full "></div>
         </div>
-
-      team:{teamName}
-      <div>{location.href}</div>
-      <button onClick={() => urlCopyHandler(location.href)}>copy</button>
-      <div className="max-w-[900px] mx-auto mt-10 mb-20">
-        <MeetingCreate></MeetingCreate>
-        <h2 className="text-2xl font-bold mt-40">会議一覧</h2>
-        <div className=" bg-neutral-300 py-12 px-12 rounded-md mt-2 space-y-10">
-          {
-            meetings? meetings.map((meeting)=>{
-              return(
-                <MeetingCard detail={meeting}/>
-              )
-            }):""
-          }
+        team:{teamName}
+        <div>{location.href}</div>
+        <button onClick={() => urlCopyHandler(location.href)}>copy</button>
+        <div className="max-w-[900px] mx-auto mt-10 mb-20">
+          <MeetingCreate></MeetingCreate>
+          <h2 className="text-2xl font-bold mt-40">会議一覧</h2>
+          <div className=" bg-neutral-300 py-12 px-12 rounded-md mt-2 space-y-10">
+            {meetings
+              ? meetings.map((meeting) => {
+                  return <MeetingCard detail={meeting} />;
+                })
+              : ""}
+          </div>
         </div>
       </div>
     </div>
